54cd27eaaf3b787a03e5fde98394effd1fc37d38
Ilya <2994020@gmail.com>
2022-02-28 09:03:48 +0300

    add folder and files hw04

---
+++ HW04/utils.py
@@ -0,0 +1,22 @@
+
+def prefilter_items(data):
+    # Уберем самые популярные товары (их и так купят)
+    popularity = data_train.groupby('item_id')['user_id'].nunique().reset_index() / data_train['user_id'].nunique()
+    popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
+    
+    top_popular = popularity[popularity['share_unique_users'] > 0.5].item_id.tolist()
+    data = data[~data['item_id'].isin(top_popular)]
+    
+    # Уберем самые НЕ популярные товары (их и так НЕ купят)
+    top_notpopular = popularity[popularity['share_unique_users'] < 0.01].item_id.tolist()
+    data = data[~data['item_id'].isin(top_notpopular)]
+    
+    # Уберем товары, которые не продавались за последние 12 месяцев
+    
+    # Уберем не интересные для рекоммендаций категории (department)
+    
+    # Уберем слишком дешевые товары (на них не заработаем). 1 покупка из рассылок стоит 60 руб. 
+    
+    # Уберем слишком дорогие товарыs
+    
+    # ...
